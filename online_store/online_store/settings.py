46936b4c25402e1e3d6f038bb4aee4729fe0c1cb
Akerke <akerke.kass@gmail.com>
2018-09-09 21:10:50 +0600

    images displayed

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+MEDIA_URL = '/products/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
