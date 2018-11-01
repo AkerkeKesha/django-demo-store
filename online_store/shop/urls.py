85a5df16d896b404efe3be340068afd131a0facc
Akerke <akerke.kass@gmail.com>
2018-11-01 20:23:05 +0600

    added url for anchor elements of templates

--- online_store/shop/urls.py
+++ online_store/shop/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.show_products, name = 'product_list'),
+    path('', views.show_products, name='product_list'),
     path('<int:product_id>', views.show_single_product, name='product_detail'),
 ]
